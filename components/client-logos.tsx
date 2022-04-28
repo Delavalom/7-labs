69ea3e380f21b386531de5bdabd080f1d6358adc
Luis Angel <luisangel@Luiss-MacBook-Pro.local>
2022-04-28 12:13:18 -0400

    adding files

---
+++ components/client-logos.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+import SliderContainer from "./slider";
+
+const ClientLogos: React.FC = () => (
+  <>
+    <SliderContainer className="" contentWidth={1290} initialOffsetX={0}>
+      <Image
+        src="/assets/trustedby/audubon.png"
+        width={150}
+        height={50}
+        alt="Audubon"
+        objectFit="contain"
+      />
+    </SliderContainer>
+  </>
+);
+
+export default ClientLogos;
---
+++ components/works.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import React from "react";
+import { Tile, TileBackground, TileContent, TileWrapper } from "./tile";
+import { WorkContainer, WorkBackground, WorkLeft, WorkRight, WorkLink } from "./work";
+
+
+const Works = () => (
+  <TileWrapper numOfPages={3}>
+    <TileBackground>
+      <WorkBackground />
+    </TileBackground>
+    <TileContent>
+      <Tile
+        page={0}
+        renderContent={({ progress }) => (
+          <WorkContainer>
+            <WorkLeft progress={progress}>
+              <div>We Built</div>
+              <div className="text-4xl md:text-5xl font-semibold tracking-tight">
+                <WorkLink href="https://pinkpanda.io">Pink Panda</WorkLink>&apos;s app
+              </div>
+            </WorkLeft>
+            <WorkRight progress={progress}>
+              <Image
+                src="/assets/works/pinkpanda.png"
+                layout="responsive"
+                width={840}
+                height={1620}
+                alt="Pink Panda"
+              />
+            </WorkRight>
+          </WorkContainer>
+      )}
+      ></Tile>
+      <Tile
+        page={1}
+        renderContent={({ progress }) => (
+          <WorkContainer>
+            <WorkLeft progress={progress}>
+              <div>We made</div>
+              <div className="text-4xl md:text-5xl font-semibold tracking-tight">
+                <WorkLink></WorkLink>
+              </div>
+              <div>Steakwallet&nbsp;faster</div>
+            </WorkLeft>
+            <WorkRight progress={progress}>
+              <Image
+                src="/assets/works/steakwallet.png"
+                layout="responsive"
+                width={840}
+                height={1620}
+                alt="Steakwallet"
+              />
+            </WorkRight>
+          </WorkContainer>
+      )}
+      ></Tile>
+      <Tile
+        page={2}
+        renderContent={({ progress }) => (
+          <WorkContainer>
+            <WorkLeft progress={progress}>
+              <div>We helped</div>
+              <div className="text-4xl md:text-5xl font-semibold tracking-tight">
+                <WorkLink></WorkLink>
+              </div>
+              <div>Showtime ship faster.</div>
+            </WorkLeft>
+            <WorkRight progress={progress}>
+              <Image
+                src="/assets/works/Showtime.png"
+                layout="responsive"
+                width={840}
+                height={1620}
+                alt="showtime"
+              />
+            </WorkRight>
+          </WorkContainer>
+      )}
+      ></Tile>
+    </TileContent>
+  </TileWrapper>
+);
+
+export default Works;
